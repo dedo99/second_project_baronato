fbe3c5a459e6aa77f6f608d1b0171ad8f235f527
dedo99 <andreadedonato99@gmail.com>
2023-06-13 16:30:18 +0200

    Cassandra

--- batch_preprocessing_analysis/scripts/main.py
+++ batch_preprocessing_analysis/scripts/main.py
@@ -1,10 +1,12 @@
 from batch_preprocessing import batch_processing_function
 from batch_analysis import batch_analysis_function
-
+import time
 
 batch_processing_function()
 
 batch_analysis_function()
+
+# time.sleep(120)
 
 print('Ciao a tutti!')
 
