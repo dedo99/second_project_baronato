0b1673da7cc42531428a3e8434152647b932cf6d
dedo99 <andreadedonato99@gmail.com>
2023-06-14 01:19:21 +0200

    Dockerfile batch e preprocessing

--- batch_preprocessing_analysis/scripts/main.py
+++ batch_preprocessing_analysis/scripts/main.py
@@ -3,19 +3,24 @@ from batch_analysis import batch_analysis_function
 import time
 from pyspark.sql import SparkSession
 import argparse
+import pandas as pd
+import pyspark.pandas as ps
 
-parser = argparse.ArgumentParser()
-parser.add_argument("--input_path", type=str, help="Input file path")
-args = parser.parse_args()
-input_filepath = args.input_path
+psdf2 = ps.DataFrame({'id': [5, 4, 3]})
 
-spark = SparkSession.builder.appName("FIRST_JOB").getOrCreate()
+print(psdf2)
+# parser = argparse.ArgumentParser()
+# parser.add_argument("--input_path", type=str, help="Input file path")
+# args = parser.parse_args()
+# input_filepath = args.input_path
 
-input_RDD = spark.sparkContext.textFile(input_filepath).cache()
+# spark = SparkSession.builder.appName("FIRST_JOB").getOrCreate()
 
-#first_ten = input_RDD.filter(lambda row: row[0]>= 1451624400 and row[0]<= 1451624410)
+# input_RDD = spark.sparkContext.textFile(input_filepath).cache()
 
-print(input_RDD.collect())
+# first_ten = input_RDD.filter(lambda row: row[0]>= 1451624400 and row[0]<= 1451624410)
+
+# print(input_RDD.collect())
 
 batch_processing_function()
 
