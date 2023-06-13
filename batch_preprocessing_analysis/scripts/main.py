defaa2654528c6cd75ed713b04501e433cf17c07
piebaroni <pietroba99@gmail.com>
2023-06-13 16:59:51 +0200

    gitignore2

--- batch_preprocessing_analysis/scripts/main.py
+++ batch_preprocessing_analysis/scripts/main.py
@@ -1,21 +1,6 @@
 from batch_preprocessing import batch_processing_function
 from batch_analysis import batch_analysis_function
 import time
-from pyspark.sql import SparkSession
-import argparse
-
-parser = argparse.ArgumentParser()
-parser.add_argument("--input_path", type=str, help="Input file path")
-args = parser.parse_args()
-input_filepath = args.input_path
-
-spark = SparkSession.builder.appName("FIRST_JOB").getOrCreate()
-
-input_RDD = spark.sparkContext.textFile(input_filepath).cache()
-
-#first_ten = input_RDD.filter(lambda row: row[0]>= 1451624400 and row[0]<= 1451624410)
-
-print(input_RDD.collect())
 
 batch_processing_function()
 
